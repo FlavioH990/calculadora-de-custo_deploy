436038ec371e1af3147ee15e97e79159f3a9b990
Flavio Henrique <fhs5895santos@outlook.com>
2025-10-05 17:15:58 -0300

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vyyzbxghpbexeubbghre.supabase.co' // substitua pela sua URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5eXpieGdocGJleGV1YmJnaHJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwMTg4NjEsImV4cCI6MjA3MDU5NDg2MX0.QW7tqi7xf6YANyaSTTrqPZoiloWZXI_nwTdCn9sF_R0' // substitua pela sua anon key
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
